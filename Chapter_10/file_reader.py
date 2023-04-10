5c58e71971018d77052c03fdcf00841f92048dea
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2023-04-10 23:16:53 +0430

    splitlines method

--- Chapter_10/file_reader.py
+++ Chapter_10/file_reader.py
@@ -1,6 +1,9 @@
 
 from pathlib import Path
 
-path = Path('text_files/pi_digits.txt')
+path = Path('pi_digits.txt')
 contents = path.read_text()
-print(contents)
+
+lines = contents.splitlines()
+for line in lines:
+    print(line)
