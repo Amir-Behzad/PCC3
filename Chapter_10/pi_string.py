c403f623f1a0a834a23c317f7125abc13bef06e0
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2023-04-11 15:35:33 +0430

    adds lstrip method

--- Chapter_10/pi_string.py
+++ Chapter_10/pi_string.py
@@ -6,7 +6,7 @@ contents = path.read_text()
 lines = contents.splitlines()
 pi_string = ''
 for line in lines:
-    pi_string += line
+    pi_string += line.lstrip()
 
 print(pi_string)
 print(len(pi_string))
