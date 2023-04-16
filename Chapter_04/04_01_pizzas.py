903eedf417ed45f8337ada8aec39ee9b5bd23a37
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2023-04-16 22:22:06 +0430

    excs of chapter 4

---
+++ Chapter_04/04_01_pizzas.py
@@ -0,0 +1,12 @@
+pizza_names = [
+    'Veggie',
+    'Pepperoni ',
+    'Meat',
+    'Margherita',
+    'BBQ Chicken',
+    'Chicago',
+]
+
+for pizza in pizza_names:
+  text = f"I like {pizza} pizza."
+  print(text)
