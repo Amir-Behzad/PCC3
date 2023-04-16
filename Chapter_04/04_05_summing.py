24eba12c329fed7816a5c3f0219e208490f610e1
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2023-04-16 22:42:59 +0430

    adds 04_05 & 04_06

---
+++ Chapter_04/04_05_summing.py
@@ -0,0 +1,11 @@
+print()
+
+num_list = list(range(1, 1_000_000))
+
+print("Min:", min(num_list))
+
+print("Max:", max(num_list))
+
+print("Sum:", sum(num_list))
+
+print()
---
+++ Chapter_04/04_06_odd_numbers.py
@@ -0,0 +1,11 @@
+print()
+
+odd_nums = list(range(1, 20, 2))
+print(odd_nums)
+
+print()
+
+for item in odd_nums:
+    print(item)
+
+print()
