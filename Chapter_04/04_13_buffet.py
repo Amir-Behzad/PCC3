5e0e97e88532e42aa7f9629154590694d955aab9
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2023-04-17 10:14:52 +0430

    solves 04_13

---
+++ Chapter_04/04_13_buffet.py
@@ -0,0 +1,31 @@
+buffet_menu = (
+    'kebab',
+    'omlet',
+    'dizzy',
+    'kotlet',
+    'falafel',
+)
+
+print()
+
+print('Original menu: ')
+for food in buffet_menu:
+    print(food)
+    
+print()
+# buffet_menu[1] = 'koobideh'
+
+buffet_menu = (
+    'koobideh',
+    'omlet',
+    'dizzy',
+    'bandari',
+    'falafel',
+)
+
+print()
+print("New menu:")
+for food in buffet_menu:
+    print(food)
+
+print()
