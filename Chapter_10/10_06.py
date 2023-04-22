787af21afa288699634594c1ab15cadbcb321c28
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2023-04-22 23:44:38 +0430

    solved 10_06

---
+++ Chapter_10/10_06.py
@@ -0,0 +1,12 @@
+try:
+    first_number = int(input("Please type the first number: "))
+    second_number = int(input("Please type the second number: "))
+    sum = first_number + second_number
+    print(
+        f"\n{first_number} + {second_number} =",
+        sum,
+        "\n"
+    )
+
+except:
+    print("Please type a proper number")
