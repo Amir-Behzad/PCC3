ffb93eed2443fc9346b311b4022563909ddbb616
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2023-04-25 00:38:54 +0430

    solves 10_09.py

---
+++ Chapter_10/10_09.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+print()
+
+try:
+  cats_path = Path('./cat.txt')
+  cats = cats_path.read_text().splitlines()
+
+  for cat in cats:
+      print("Cat's name:", cat)
+  
+except FileNotFoundError:
+  print('\nFile not found for cats.\n')
+  
+print()
+  
+try:
+  dogs_path = Path('./dogs.txt')
+  dogs = dogs_path.read_text().splitlines()
+
+  for dog in dogs:
+      print("Dog's name:", dog)
+
+except FileNotFoundError:
+  print('\nFile not found for dogs.\n')
+  
+print()
