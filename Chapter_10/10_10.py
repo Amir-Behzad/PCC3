e67c94d2f232ffba36c0be53706a0e9435e43cd8
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2023-04-25 17:42:27 +0430

    10_10 with error handling

--- Chapter_10/10_10.py
+++ Chapter_10/10_10.py
@@ -1,24 +1,27 @@
 from pathlib import Path
 
-path = Path('./qoran.txt')
+try:
+    path = Path('./qorran.txt')
 
-qoran_text = path.read_text()
+    qoran_text = path.read_text()
 
-The_count = qoran_text.count('The')
-the_count = qoran_text.count('the')
-just_the_count = qoran_text.lower().count('the ')
+    The_count = qoran_text.count('The')
+    the_count = qoran_text.count('the')
+    just_the_count = qoran_text.lower().count('the ')
 
-print(
-    "'The' counter:",
-    The_count
-)
+    print(
+        "'The' counter:",
+        The_count
+    )
 
-print(
-    "'the' counter:",
-    the_count
-)
+    print(
+        "'the' counter:",
+        the_count
+    )
 
-print(
-    "'the ' counter:",
-    just_the_count
-)
+    print(
+        "'the ' counter:",
+        just_the_count
+    )
+except FileNotFoundError:
+    print("File not found.")
