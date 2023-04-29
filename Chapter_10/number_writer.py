ddd3ff84cacb4340c1c1627cc3e3955b84580d8d
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2023-04-29 11:24:30 +0430

    modified number_writer.py

--- Chapter_10/number_writer.py
+++ Chapter_10/number_writer.py
@@ -4,5 +4,5 @@ import json
 numbers = [2, 3, 5, 7, 11, 13]
 names = ['Amir', 'James', 'Lana', 'John']
 path = Path('numbers.json')
-contents = json.dumps(numbers + names)
+contents = json.dumps(numbers)
 path.write_text(contents)
