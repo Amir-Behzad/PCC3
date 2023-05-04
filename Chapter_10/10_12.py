70535be34b8dc32d82da59274b5188c80642f22e
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2023-05-04 12:06:50 +0430

    completed 10_12.py

--- Chapter_10/10_12.py
+++ Chapter_10/10_12.py
@@ -1,16 +1,34 @@
 from pathlib import Path
 import json
 
-path = Path('users.json')
-numbers_list = [2, 5, 1, 6, 3]
-content = json.dumps(numbers_list)
+path = Path('numbers.json')
+
+prompt = "Type 'q' to exit\n"
+prompt += "Your favorite number: "
+
+content = path.read_text()
+numbers = json.loads(content)
+numbers = list(numbers)
+
+while True:
+    number = input(prompt)
+    if number == "q":
+        break
+    else:
+        try:
+            number = int(number)
+            if number not in numbers:
+                numbers.append(number)
+            else:
+                print(f"{number} was already added.")
+        except:
+            print("wrong input.")
+
+content = json.dumps(numbers)
 path.write_text(content)
 
-contents = path.read_text()
-numbers = json.loads(contents)
 print('\nNumbers: ')
 for number in numbers:
     print(number)
     
 print()
-
