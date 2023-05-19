90d840322f81277bca4a770ecff0327a6777a91c
Amir-Behzad <a.behzad.ebr@gmail.com>
2023-05-19 18:25:07 +0330

    10_06.py improved with a while loop

--- Chapter_10/10_06.py
+++ Chapter_10/10_06.py
@@ -1,14 +1,23 @@
-print('Type two numbers to get the sum of them.')
-first_number = input("First number: ")
-second_number = input("Second number: ")
+while True:
+    prompt = "Type 'q' to exit. "
+    print('Type two numbers to get the sum of them.')
+    first_number = input(prompt + "\nFirst number: ")
+    if first_number == 'q':
+        break
+    second_number = input(prompt + "\nSecond number: ")
+    if second_number == 'q':
+        break
 
-try:
-    sum = int(first_number) + int(second_number)
-    print(
-        f"\n{first_number} + {second_number} =",
-        sum,
-        "\n"
-    )
+    try:
+        sum = int(first_number) + int(second_number)
 
-except ValueError:
-    print("Please type valid numbers.")
+    except ValueError:
+        print("Please type valid numbers.")
+
+    else:
+        print(
+            f"\n{first_number} + {second_number} =",
+            sum,
+            "\n"
+        )
+        
