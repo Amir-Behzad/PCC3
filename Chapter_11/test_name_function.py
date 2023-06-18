5e0ff54c1d8797526f484db1a72348c43f9993d5
Amir Behzad Ebrahimi <a.behzad.ebr@gmail.com>
2023-06-18 08:12:46 +0330

    add test file

---
+++ Chapter_11/test_name_function.py
@@ -0,0 +1,7 @@
+
+from name_function import get_formatted_name
+
+def test_first_last_name():
+    """Do names like 'Janis Joplin' work?"""
+    formatted_name = get_formatted_name('janis', 'joplin')
+    assert formatted_name == 'Janis Joplin'
